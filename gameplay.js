2d64fe0b986c3c9ee8b77e0abc1070f25abb57e3
Ruth <ruth.murphy1@marist.edu>
2016-11-25 23:38:01 -0500

    Split out javacode to relative files
    Separated code sections to separate files and updated Map to open in a
    new window for those stuck in the game play.

---
+++ gameplay.js
@@ -0,0 +1,127 @@
+//
+// Final Project Game V1.0
+// Filename: gameplay.js
+// 
+// Ruth Murphy - CMPT 120 Fall 2016
+// Final Project general game play focused file
+// Control HTML file is "Final Project-Murphy.html"
+// Last Update: November 25, 2016
+
+    //
+    // List of Global Variables for Game
+    //         
+         var currentLoc = 0;
+         var score = 0;
+         var directionErrorCount = 0;
+         var limit = 5;
+	//
+    // Command Button handler for input by user.
+    //=======================================================================
+    // 1) trim off extra spaces
+    // 2) make sure all charaters are lower case
+    // 3) Evaluate if north, south, east or west was entered or something else
+    // 4) if direction was typed in, starting at position 0 on the line, grab first character
+    // 5) Then trigger the button function that handles the movement to the locations (reuse functions)
+    // NOTE: Script will take user input and validate it is a direction.
+    // I have scripted this to allow for entry of more than just a single letter  (N, W, E, S, n, e ,w, s).
+    // it will only validate to these single letter directions.
+    //  Flexability was added for the user though.
+	// 
+	// Additional buttons allow for looking at the players inventory and taking items in the game.
+	// These commands also allow single letter commands as above.
+	//  Allows upper and lower case, but converts to lowercase for the script
+    //
+    
+	function buttonGo() {
+        var command = document.getElementById("txtCommand").value;
+            command = command.trim();
+            command = command.toLowerCase();
+              if (command == "north" || command == "west" || command == "south" || command == "east" 
+		        || command == "help" || command == "take" || command == "invent") {
+                      command = command.substr(0,1);
+                 }
+               if (command == "n")  {
+                  btnGo_North_click();
+                  }  else if (command == "s") {
+                         btnGo_South_click();
+                    }  else if (command == "w") {
+                          btnGo_West_click();
+                      }  else if (command == "e") {
+                            btnGo_East_click();
+				    	}   else if (command == "h") {
+						      btnHelp();
+				    	 }   else if (command == "i") {
+						       btnInv();
+						  }    else if (command == "t") {
+							     btnTake();
+					       }    else displayMessage("Please try again." +"\n" + "Type 'help' for valid commands." 
+                                     + "\n" + " or press a button below");
+		                }
+    //
+	// Set up Help Button behavior
+	//=========================================
+	// Function to show the user what commands are allowed and the Goal of the game.
+	//
+		 
+	function btnHelp() {
+	  displayMessage("Valid moves are: N, S, E, W, n, s, e, w." + 
+	                "\n" + " or press a directional buttons below"
+					+ "\n\n" + "Valid commands are 'Take','Invent' or 'Help'."
+					+ "\n\n" + "Press the 'Inventory' button to see the contents of your bag."
+					+ "\n" + "Press the 'Take' button to pick up items."
+					+ "\n" + "Press the 'Help' button to see this message."
+					+ "\n\n" + "Objectives: " + "1) Rescue the Women; " + "2) Get the Key; "
+					+ "3) Find the book; " + "4) Keep up your strength;" + "Score at least 50 points");
+	            }
+    //
+    // Initialization function for executing when page (re)loads
+    //
+    function init() {
+	   desc = "You are standing in front of a yellow house" + "\n" + "\n" + "Your Objectives are: "
+	   +"\n" + "1) Rescue the Women" + "\n" + "2) Get the Key" + "\n" + "3) Find the book" 
+	   +"\n" + "4) Keep up your strength" + "\n" +"5) Score at least 50";
+        displayMessage(desc);
+		// Inital Direction button status is set below 
+	     document.getElementById("btnGo_S").disabled = true;
+	     document.getElementById("btnGo_W").disabled = true;
+	     document.getElementById("btnGo_E").disabled = true;
+	     document.getElementById("btnGo_N").disabled = false;
+       }
+       
+    // This is an empty array so we can fill it for the player.  
+    // They will be able to tell what they have picked up during game play.
+    // When needed, we can add or remove items easier.
+   
+   var inventory = new Array();
+
+   //
+    // Function for the inventory button to display the current inventory to the user. 
+    // Also called when user types in the inventory commands that are allowed.
+    //
+
+	function btnInv(){
+	    var msg = "Inventory: " + inventory;
+        displayMessage(msg);
+      }
+
+	  // Function for the "Take" button
+    // Puts items into inventory, notifies the user that the item has been taken and updates the location description.
+    // The additional text related to the item is removed and description is updated for the player when they return to the room.
+		
+    function btnTake(item) {
+	   var loc = locations[currentLoc];
+	   if (loc.hasItem === false){
+	   displayMessage("There is nothing to take here");
+	   } else if (loc.hasItem === true){
+	     inventory.push(items[currentLoc].name);
+		 items[currentLoc].isTaken = true;
+		 loc.hasItem = false;
+		 loc.desc=loc.descAfter
+		 displayMessage(items[currentLoc].name + " "+ "Taken.");
+	   	}
+	}			   
+
+    function displayMessage(msg) {
+        var target = document.getElementById("taMain");
+        target.value = msg + "\n\n\n" + target.value ;  
+	} 
---
+++ navigate.js
@@ -0,0 +1,131 @@
+//
+// Final Project Game V1.0
+// Filename: navigate.js
+// 
+// Ruth Murphy - CMPT 120 Fall 2016
+// Final Project Navigation focused file
+// Control HTML file is "Final Project-Murphy.html"
+// Last Update: November 25, 2016
+
+   //
+    // List of Global Variables for Game
+    //         
+         var currentLoc = 0;
+         var score = 0;
+         var directionErrorCount = 0;
+         var limit = 5;
+	//
+    // Array for the movement of the player.  This shows all of the valid moves allowed from each location.
+	// This is a location array based on direction array (Location to indv direction)
+    //		
+    var validMoves = [
+                        // N S E W 
+       /*Location[0] */   [1,0,0,0],
+       /*Location[1] */   [1,0,5,3],
+       /*Location[2] */   [6,4,3,10],
+       /*Location[3] */   [3,5,1,2],
+       /*Location[4] */	  [2,9,7,5],
+       /*Location[5] */	  [3,8,4,1],
+       /*Location[6] */   [6,2,6,6],
+       /*Location[7] */	  [7,7,7,4],
+       /*Location[8] */	  [5,8,8,8],
+       /*Location[9] */	  [4,9,9,9],
+       /*Location[10]*/	  [10,10,2,10]
+	        	       ];
+
+     // Begin the coding for the use of the buttons in the game.
+     // Evaluates what the current valid moves are depending on button (or command) pressed/issued on the screen.
+     // Once the location is evaluated then the look function is executed
+    function btnGo_North_click() {
+         currentLoc = validMoves[currentLoc][0];
+		 look();
+		 }
+
+    function btnGo_South_click() {
+        currentLoc = validMoves[currentLoc][1];
+		 look();
+         } 
+	
+	function btnGo_East_click() {
+        currentLoc = validMoves[currentLoc][2];
+		 look();
+	      }
+    		
+	function btnGo_West_click() {
+        currentLoc = validMoves[currentLoc][3];
+		 look();
+	     }		
+	
+    // Variable & Array for the navigation buttons.
+	// This will allow the buttons to be disabled as player moves through the game
+    var navBtns = new Array ("btnGo_N","btnGo_S","btnGo_E","btnGo_W" );
+
+                       
+	//This is the array for the navigation buttons to use  - on and off as player travels.
+	var navButtons_switch = new Array(/*    N   S   E   W */
+                                    /*0*/  [0,  1,  1,  1],
+                                    /*1*/  [1,  0,  0,  0],
+                                    /*2*/  [0,  0,  0,  0],
+                                    /*3*/  [1,  0,  0,  0],
+                                    /*4*/  [0,  0,  0,  0],
+                                    /*5*/  [0,  0,  0,  0],
+                                    /*6*/  [1,  0,  1,  1],
+                                    /*7*/  [1,  1,  1,  0],
+                                    /*8*/  [0,  1,  1,  1],
+                                    /*9*/  [0,  1,  1,  1],
+                                    /*10*/ [1,  1,  0,  1]
+                                         );
+
+       // Set up of locations that will be displayed for the user.
+       //=========================================================
+       // Function look is the coordinator of when a location will be called.
+	   // it will display the location text and the players current score.
+	   // Scoring is done based on the players having been in the room already or not.
+	   //   - player gets 5 points each time they visit a room for the first time.
+       // Used with button functions/arrays and message functions to display current location desription to user.
+	   // Buttons are also disabled via this function.
+       //
+    function look() {
+        var loc = locations[currentLoc];
+		var desc = loc.name + ": " + loc.desc + "\n";
+         if (loc.visited === false){
+			score=score + 5;
+			loc.visited=true;
+			 }
+				for (var i = 0; i < navBtns.length; i++) {
+	              var btnDisable = 0;
+	                btnDisable = navButtons_switch[currentLoc][i];
+	              if (btnDisable === 1) {
+	               document.getElementById(navBtns[i]).disabled = true;
+	             } else {
+	               document.getElementById(navBtns[i]).disabled = false;
+	             }
+	            }
+			displayMessage(desc + "Your Score is: " + score +"\n");
+		}
+		 
+
+				
+	//
+    // Function to check an error in direction.  Ie: a wall has been reached
+    // ========================================================================
+    // The first time a user goes to a location that is not valid, they get 
+    // the message that they should try another direction.  After multiple times (variable "limit") with no valid
+    // direction, a new message is shown to rethink what they are doing.
+    // NOTE:  The logic in this game with the buttons now prevents this set of code from being executed.
+	// Leaving code as a precaution and in case browser updates allow breaking out of button functions.
+    //
+    function directionError() {
+         directionErrorCount = directionErrorCount + 1;
+            if (directionErrorCount < limit) {
+               displayMessage("You might want to try another direction.");
+            } else {
+               displayMessage("Umm, Where are you going?  Pick another direction.");
+            }            
+        }
+
+    //
+    // Function for displying the message correctly (location and score) as we iterate through the game
+    //=================================================================================================
+	// Includes the value of the score.
+    //
